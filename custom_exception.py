45da541a68ac76a8bc51533b13da73af5a63b042
Dawittsegaye12 <dtsegaye560@gmail.com>
2024-11-03 03:38:24 -0800

    custom_exception.py

--- custom_exception.py
+++ custom_exception.py
@@ -1,1 +1,20 @@
-print("hello world")
+import sys
+#import traceback
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = self.error_message_detail(error_message, error_detail)
+
+    def error_message_detail(self, error_message, error_detail: sys):
+        _, _, exc_tb = sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_no = exc_tb.tb_lineno
+
+        error_message_detail = f"The error occurred in {file_name} on line number {line_no}. The error is: {error_message}"
+        return error_message_detail
+
+    def __str__(self):
+        return self.error_message
+    
+    print("hello world")
